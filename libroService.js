d937871557836e4557489d954bae878bb3443c14
Gaston-Sartini <38993215@comunidadort.edu.ar>
2026-09-03 18:55:59 -0300

    feat: implementar agregarLibro

--- libroService.js
+++ libroService.js
@@ -37,7 +37,34 @@ async function main(){
     console.log(await buscarPorId(3));
     console.log(await buscarLibrosPorAutor("Antoine de Saint-Exupery"));
     console.log(await librosEnStock());
+    const libro = {
+        "titulo": "Volver al futuro",
+        "autor": "Peter Parket",
+        "stock": 4
+    };
+    console.log(await agregarLibro(libro));
 }
 
+/*
+3. Agregar una función nueva `async function agregarLibro(libro)` que:
+   - Lea el archivo actual.
+   - Le agregue el libro nuevo (con un `id` que sea `Math.max(...ids) + 1`, o `1` si está vacío).
+   - Escriba el array completo de vuelta al archivo con `fs.writeFile` (usando `JSON.stringify(libros, null, 2)` para que quede legible).
+   - Devuelva el libro creado.
+*/
+
+async function agregarLibro(libro){
+    const libros = await listarLibros();
+    let nuevoId = 1;
+    if(libros.length > 0){
+        const ids = libros.map((uLibro)=>uLibro.id);
+        nuevoId = Math.max(...ids) + 1;     
+    }
+    libro.id = nuevoId;
+    const libroCompleto = { id: nuevoId, ...libro };
+    libros.push(libroCompleto);
+    await fs.writeFile(rutaArchivo, JSON.stringify(libros, null, 2));
+    return libro;
+};
 main();
 
