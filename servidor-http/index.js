372128fb1b7ae006c1b56d1830abdd454f7b3ceb
Gaston-Sartini <38993215@comunidadort.edu.ar>
2026-09-25 18:23:14 -0300

    clase 3: desafío POST /eco

--- servidor-http/index.js
+++ servidor-http/index.js
@@ -16,6 +16,24 @@ const server = http.createServer((req, res) => {
         res.statusCode = 200;
         res.setHeader("Content-Type", "application/json");
         res.end(JSON.stringify(libros));
+    } else if (method === "POST" && url === "/eco"){
+        let body = "";
+        let cuerpo = "";
+        req.on("data", (chunk) => {
+            body += chunk;
+        });
+        req.on("end", () => {
+            try {
+                cuerpo = JSON.parse(body);
+                res.statusCode = 200;
+                res.setHeader("Content-Type", "application/json");
+                res.end(JSON.stringify(cuerpo));                
+            } catch (error) {
+                res.statusCode = 400;
+                res.setHeader("Content-Type", "application/json");
+                res.end(JSON.stringify({error: "El body no es un JSON válido"}));                
+            }
+        });
     } else {
         res.statusCode = 404;
         res.setHeader("Content-Type", "application/json");
