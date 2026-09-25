830c1bfde9913f25223bbb3dcbf37819d239c81c
Gaston-Sartini <38993215@comunidadort.edu.ar>
2026-09-25 17:18:12 -0300

    clase 3: ejercicio 1 - libros en JSON con fs/promises

--- biblioteca-api/index.js
+++ biblioteca-api/index.js
@@ -1,6 +1,16 @@
-const { listarLibros, buscarPorId, buscarPorAutor, contarConStock } = require("./libroService");
+const { listarLibros, buscarPorId, buscarPorAutor, contarConStock, agregarLibro } = require("./libroService");
 
-console.log("Todos los libros:", listarLibros());
-console.log("Libro con id 3:", buscarPorId(3));
-console.log("Libros de Orwell:", buscarPorAutor("orwell"));
-console.log("Libros con stock:", contarConStock());
+async function main(){
+    try {
+        console.log("Todos los libros:", await listarLibros());
+        console.log("Libro con id 3:", await buscarPorId(3));
+        console.log("Libros de Orwell:", await buscarPorAutor("orwell"));
+        console.log("Libros con stock:", await contarConStock());
+        console.log("Agregando Libro", await agregarLibro({titulo: "El día de ayer", autor: "Yamilse Seisdesdos", stock: 7}));
+        console.log("Cantidad de libros", (await listarLibros()).length);
+    } catch (error) {
+        console.log(error);   
+    }
+}
+
+main();
--- biblioteca-api/libroService.js
+++ biblioteca-api/libroService.js
@@ -1,19 +1,39 @@
-const libros = require("./data/libros");
+const fs = require("fs/promises");
+const path = require("path");
+const RUTA = path.join(__dirname, "data", "libros.json");
 
-function listarLibros() {
+async function listarLibros() {
+  const data = await fs.readFile(RUTA, "utf-8");
+  const libros = JSON.parse(data);
   return libros;
 }
 
-function buscarPorId(id) {
+async function buscarPorId(id) {
+  const libros = await listarLibros();
   return libros.find((libro) => libro.id === id);
 }
 
-function buscarPorAutor(autor) {
+async function buscarPorAutor(autor) {
+  const libros = await listarLibros();
   return libros.filter((libro) => libro.autor.toLowerCase().includes(autor.toLowerCase()));
 }
 
-function contarConStock() {
+async function contarConStock() {
+  const libros = await listarLibros();
   return libros.filter((libro) => libro.stock > 0).length;
 }
 
-module.exports = { listarLibros, buscarPorId, buscarPorAutor, contarConStock };
+async function agregarLibro(libro){
+  const libros = await listarLibros();
+  let nuevoId = 1;
+  if(libros.length > 0){
+    const ids = libros.map((l) => l.id);
+    nuevoId = Math.max(...ids) + 1;
+  }
+  const nuevoLibro = {id: nuevoId, ...libro};
+  libros.push(nuevoLibro);
+  await fs.writeFile(RUTA, JSON.stringify(libros, null, 2));
+  return nuevoLibro;
+}
+
+module.exports = { listarLibros, buscarPorId, buscarPorAutor, contarConStock, agregarLibro };
